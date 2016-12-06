0e46625c8fcf322aa3e6fa14df2c33dfd099c4e2
Tom Gleghorn <tgleghorn@gmail.com>
2016-12-06 10:42:23 -0500

    cleanup day6 a little

--- src/day6.ts
+++ src/day6.ts
@@ -6,7 +6,7 @@ namespace Day6 {
 		return matrix[0].map((_, col) => matrix.map((row) => row[col]));
 	}
 
-	function countLetters(letters: string[]) : [string, number][] {
+	function countAndSortLetters(letters: string[]) : [string, number][] {
 		let counts = {};
 		for (let i = 0; i < letters.length; i++) {
 			if (counts[letters[i]] === undefined) {
@@ -18,45 +18,24 @@ namespace Day6 {
 		return Object.keys(counts).map(k => {
 			let count: [string, number] = [k, counts[k]];
 			return count;
-		});
-	}
-
-	function getMostCommonCharacter(counts: [string, number][]) : string {
-		return counts.sort((a, b) => {
+		}).sort((a, b) => {
 			if (a[1] === b[1]) {
 				return a[0] < b[0] ? -1 : a[0] > b[0] ? 1 : 0;
 			}
 			return a[1] < b[1] ? 1 : -1;
-		})[0][0];
+		});
 	}
 
-	function getLeastCommonCharacter(counts: [string, number][]) : string {
-		return counts.sort((a, b) => {
-			if (a[1] === b[1]) {
-				return a[0] < b[0] ? -1 : a[0] > b[0] ? 1 : 0;
-			}
-			return a[1] > b[1] ? 1 : -1;
-		})[0][0];
-	}
-
-	function decodeSignalPart1(input: string[]) : void {
+	function decodeSignal(input: string[]) : void {
 		let signal = transpose(input.map(x => x.split("")));
-		let message = signal.map(x => {
-			let counts = countLetters(x);
-			return getMostCommonCharacter(counts);
+		let message1 = signal.map(x => countAndSortLetters(x)[0][0]).reduce((a, b) => a + b);
+		let message2 = signal.map(x => {
+			let counts = countAndSortLetters(x);
+			return counts[counts.length - 1][0];
 		}).reduce((a, b) => a + b);
-		console.log(`part 1 message: ${message}`);
+		console.log(`part 1 message: ${message1}`);
+		console.log(`part 2 message: ${message2}`);
 	}
 
-	function decodeSignalPart2(input: string[]) : void {
-		let signal = transpose(input.map(x => x.split("")));
-		let message = signal.map(x => {
-			let counts = countLetters(x);
-			return getLeastCommonCharacter(counts);
-		}).reduce((a, b) => a + b);
-		console.log(`part 1 message: ${message}`);
-	}
-
-	decodeSignalPart1(getInput("day6.txt"));
-	decodeSignalPart2(getInput("day6.txt"));
+	decodeSignal(getInput("day6.txt"));
 }
