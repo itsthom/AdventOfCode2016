fefc6695be59b8a87959191b9f7b5029128dcf0b
thom gleghorn <thom gleghorn>
2016-12-09 21:23:39 -0500

    day9 part 2

--- src/day9.ts
+++ src/day9.ts
@@ -1,15 +1,15 @@
 import { getInput } from "./reader";
 
 namespace Day9 {
-	enum inputMode { normal,  instruction, repeating }
+	enum inputMode { normal, instruction, repeating }
 
-	function decompress(input: string): string {
+	function decompressPart1(input: string): string {
 		let output = "";
 		let buffer = "";
 		let repeatLength = 0;
 		let repeatCount = 0;
 		let state = inputMode.normal;
-		
+
 		for (let i = 0; i < input.length; i++) {
 			let c = input[i];
 			switch (state) {
@@ -47,19 +47,51 @@ namespace Day9 {
 		return output;
 	}
 
-	function test(input: string, expected: string) {
-		let result = decompress(input);
-		if (result === expected)
-			console.log(`pass result: "${result}"`);
-		else
-			console.log(`FAIL -> actual: "${result}", expected: "${expected}"`);
+	function decompressPart2(input: string): number {
+		let lengthOfMessage = 0;
+		let buffer = "";
+		let repeatLength = 0;
+		let repeatCount = 0;
+		let state = inputMode.normal;
+		for (let i = 0; i < input.length; i++) {
+			let c = input[i];
+			switch (state) {
+				case inputMode.normal:
+					if (c === "(") {
+						state = inputMode.instruction;
+					} else {
+						lengthOfMessage++;
+					}
+					break;
+				case inputMode.instruction:
+					if (c === "x") {
+						repeatLength = parseInt(buffer);
+						buffer = "";
+					} else if (c === ")") {
+						repeatCount = parseInt(buffer);
+						buffer = "";
+						state = inputMode.repeating;
+					} else {
+						buffer += c;
+					}
+					break;
+				case inputMode.repeating:
+					buffer += c;
+					repeatLength--;
+					if (repeatLength === 0) {
+						lengthOfMessage += decompressPart2(buffer) * repeatCount;
+						buffer = "";
+						repeatCount = 0;
+						state = inputMode.normal;
+					}
+					break;
+			}
+		}
+		return lengthOfMessage;
 	}
-	// test("ADVENT", "ADVENT");
-	// test("A(1x5)BC", "ABBBBBC");
-	// test("(3x3)XYZ", "XYZXYZXYZ");
-	// test("A(2x2)BCD(2x2)EFG", "ABCBCDEFEFG");
-	// test("(6x1)(1x3)A", "(1x3)A");
-	// test("X(8x2)(3x3)ABCY", "X(3x3)ABC(3x3)ABCY");
-	let output = decompress(getInput("day9.txt")[0]);
-	console.log(`decompressed length: ${output.length}`);
+
+	let output1 = decompressPart1(getInput("day9.txt")[0]);
+	let output2 = decompressPart2(getInput("day9.txt")[0]);
+	console.log(`decompressed length part 1: ${output1.length}`);
+	console.log(`decompressed length part 2: ${output2}`);
 }
